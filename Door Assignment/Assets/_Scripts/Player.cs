6cd1a11679ca8da134f564bd6a6b598811d4c1ee
Dan/LT <browning.dan@gmail.com>
2018-12-10 02:15:39 -0500

    finished project

---
+++ Door Assignment/Assets/_Scripts/Player.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+    public bool isEmpty;
+    public bool RL;
+    public bool platform;
+
+
+    public GameObject launcher;
+    public Transform launchPoint;
+    public GameObject rocket;
+    public GameObject grating;
+    public Transform grateSpot;
+
+	// Use this for initialization
+	void Start ()
+    {
+        isEmpty = true;
+        RL = false;
+        platform = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(1) && !isEmpty)
+        {
+                if (RL)
+                {
+                Instantiate(rocket, launchPoint.position, transform.rotation);
+                //Instantiate(rocket, launchPoint);
+                    launcher.SetActive(false);
+                    RL = false;
+                    isEmpty = true;
+                }
+
+                /*else if (platform)
+                {
+                    grating.SetActive(false);
+                    platform = false;
+                    isEmpty = true;
+                }*/
+        }
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetMouseButtonDown(0) && isEmpty)
+        {
+            if (other.gameObject.tag == "Forcefield")
+            {
+                Destroy(other.gameObject);
+            }
+
+            if (other.gameObject.tag == "Rocket Launcher")
+            {
+                Destroy(other.gameObject);
+                launcher.SetActive(true);
+                isEmpty = false;
+                RL = true;
+            }
+
+            if (other.gameObject.tag == "Platform")
+            {
+                Destroy(other.gameObject);
+                grating.SetActive(true);
+                isEmpty = false;
+                platform = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Deathbox")
+        {
+            SceneManager.LoadScene("Door Level");
+            //SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+        }
+
+        if (other.gameObject.tag == "Crossing" && platform)
+        {
+            Instantiate(grating, grateSpot);
+
+            grating.SetActive(false);
+            platform = false;
+            isEmpty = true;
+        }
+    }
+}
---
+++ Door Assignment/Assets/_Scripts/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    public float moveSpeed;
+    private Vector3 direction;
+    private Rigidbody _rb;
+
+    //private Vector3 direction;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //transform.position = transform.Translate.;
+        //_rb.AddForce(direction * -moveSpeed, ForceMode.Impulse);
+        transform.position += Time.deltaTime * moveSpeed * transform.forward;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+            if (other.gameObject.tag == "Door")
+            {
+                Destroy(other.gameObject);
+            }
+
+        Destroy(gameObject);
+    }
+
+}
